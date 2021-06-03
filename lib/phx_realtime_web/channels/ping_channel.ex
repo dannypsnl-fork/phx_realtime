77e055d12768d0256ee6d250110b44e09c72cd62
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-03 18:56:05 +0800

    channel/ping: ack

--- lib/phx_realtime_web/channels/ping_channel.ex
+++ lib/phx_realtime_web/channels/ping_channel.ex
@@ -5,6 +5,9 @@ defmodule PhxRealtimeWeb.PingChannel do
     {:ok, socket}
   end
 
+  def handle_in("ping", %{"ack" => ack}, socket) do
+    {:reply, {:ok, %{ping: ack}}, socket}
+  end
   def handle_in("ping", _payload, socket) do
     {:reply, {:ok, %{ping: "pong"}}, socket}
   end
