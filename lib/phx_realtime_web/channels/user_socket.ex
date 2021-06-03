3468e42f2ef36259065f198eaa1876b88431a96a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-03 15:05:20 +0800

    channel: ping pong

--- lib/phx_realtime_web/channels/user_socket.ex
+++ lib/phx_realtime_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PhxRealtimeWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PhxRealtimeWeb.RoomChannel
+  channel "ping", PhxRealtimeWeb.PingChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
