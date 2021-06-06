45d8f328d3be083ce685fbe0901ef20465cd3e78
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-06 15:32:03 +0800

    comment recurringChannel

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -15,18 +15,11 @@ let authSocket = new Socket("/auth_socket", {
 authSocket.onOpen(() => console.log("authSocket connected"));
 authSocket.connect();
 
-const recurringChannel = authSocket.channel("recurring");
-recurringChannel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
-  });
-recurringChannel.on("new_token", (payload) =>
-  console.log("received new auth token", payload)
-);
+// const recurringChannel = authSocket.channel("recurring");
+// recurringChannel.join();
+// recurringChannel.on("new_token", (payload) =>
+//   console.log("received new auth token", payload)
+// );
 
 const dupeChannel = authSocket.channel("dupe");
 dupeChannel.on("number", (payload) => console.log("new number", payload));
